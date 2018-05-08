e413884c89d874a971cfd9a8cd676e1a8181e307
serjt12 <jandreus87@hotmail.com>
2018-05-08 10:02:15 -0500

    background video

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
+import BackGroundVideo from './BackGroundVideo'
+
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <p>
-          HOME
-        </p>
+      <div>
+        <BackGroundVideo />
       </div>
     )
   }
